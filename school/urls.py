d1f494f0f51587bccd1f16b5aa84ebe2306472c2
lapiad <lapiadlei@gmail.com>
2025-03-04 17:43:25 +0800

    inital

---
+++ school/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from student.views import student_list, student_create, student_update, student_delete
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('student/', student_list, name='student_list'),
+    path('',student_create, name='student_create'),
+    path('<int:pk>/edit/',student_update, name='student_update'),
+    path('<int:pk>/edit/',student_delete, name='student_delete'),
+    
+]
---
+++ student/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField(default=0)
+    date_of_birth = models.DateField()
+    gender = models.CharField(
+        max_length=10,
+        choices=[('Male', 'Male'), ('Female', 'Female')]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+# Create your models here.
